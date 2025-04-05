951b159131f2f175b18740848de63dda051b8266
Abdulsamad <abdulsamadhussaini001@gmail.com>
2025-04-05 16:59:33 +0100

    complete integration of geo location and location on the map

--- script.js
+++ script.js
@@ -1,10 +1,86 @@
-var map = L.map("map", {
-  center: [51.505, -0.09],
-  zoom: 13,
-});
+// var map = L.map("map", {
+//   center: [51.505, -0.09],
+//   zoom: 13,
+// });
+let map = L.map("map").setView([0, 0], 2);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+let currentMarker = null;
+
+// var map = L.map("map", {
+//   center: [51.505, -0.09],
+//   zoom: 13,
+// });
+
+var latlng = L.latLng(37.25, 119.3);
+
+const ipInput = document.querySelector(".input-field");
+const ipValue = ipInput.value.trim();
+
+const searchButton = document.querySelector(".button");
+const ipAddress = document.querySelector(".ip-address");
+const location2 = document.querySelector(".location");
+const utc = document.querySelector(".utc");
+const isp = document.querySelector(".isp");
+
+const URL = "https://geo.ipify.org/api/v2/country,city";
+const API_KEY = "RC1Irf8bP8U862axQJzxkZlEGYLnS";
+let debounceTimeout;
+let ip2 = "8.8.8.8";
+
+async function getIpAddress(ip) {
+  try {
+    ip2 = ip;
+    const response = await fetch(
+      `${URL}?apiKey=at_${API_KEY}&ipAddress=${ip2}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const { lat, lng, country, region, timezone } = data.location;
+
+    if (!lat || !lng) {
+      throw new Error("Latitude or longitude not found in API response");
+    }
+
+    if (currentMarker) {
+      map.removeLayer(currentMarker);
+    }
+
+    // Add a new marker at the IP's location
+    currentMarker = L.marker([lat, lng]).addTo(map);
+    currentMarker.bindPopup(`<b>${country}</b><br>${region}`).openPopup();
+
+    // Center the map on the new location
+    map.setView([lat, lng], 10); // Zoom level 10 for a closer view
+
+    ipAddress.textContent = data.ip;
+    location2.textContent = `${country}, ${region}`;
+    utc.textContent = `UTC ${timezone}`;
+    isp.textContent = data.isp;
+    console.log(data);
+    console.log(lat);
+  } catch (error) {
+    console.error("API request failed:", error);
+  }
+}
+getIpAddress(ip2);
+
+searchButton.addEventListener("click", () => {
+  console.log("Search button clicked");
+  const ipAddress = ipInput.value.trim();
+
+  if (!ipAddress) {
+    console.error("Please enter an IP address");
+    return;
+  }
+  getIpAddress(ipAddress);
+});
